7c41074867a9cb824ed68cca1741eb0c164d0752
Kukuh182 <kukuhpr21@gmail.com>
2024-06-13 06:11:15 +0700

    init

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        @vite(['resources/css/app.css','resources/js/app.js'])
+    </head>
+    <body class="bg-white">
+        <div class="flex flex-row h-screen">
+            <x-sidebar/>
+
+            {{-- main --}}
+            <div class="flex flex-col w-full bg-green-400">
+                <x-navbar/>
+            </div>
+            {{-- main --}}
+        </div>
+    </body>
+</html>
---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,3 @@
+<div class="flex items-end h-16 bg-slate-200 p-2">
+    navbar
+</div>
